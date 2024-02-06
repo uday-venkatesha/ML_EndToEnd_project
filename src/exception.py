d805513899817513d83e41e0773f54a1a7b1839c
uday-venkatesha <udayvenkatesh2015@gmail.com>
2024-02-06 13:47:06 -0600

    exception file changes

--- src/exception.py
+++ src/exception.py
@@ -5,10 +5,11 @@ def error_message_details(error, error_detail:sys):
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="error occoured in python script name[{0}] line number [{1}] error message[{2}]".format()
     file_name,exc_tb.tb_lineno,str(error)
+    return error_message
     
 class CustomException(Exception):
     def __init__(self,error_message,error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_detail=error_detail)
         
     def __str__(self):
